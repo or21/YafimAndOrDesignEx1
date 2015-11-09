9dd545477a72ff0c202137641676cbecc287c4ee
or21 <orbrand21@gmail.com>
2015-11-09 16:04:09 +0200

    full

--- AppUI/Form1.cs
+++ AppUI/Form1.cs
@@ -13,25 +13,15 @@ namespace AppUI
     // Test comment to commit and push
     public partial class Form1 : Form
     {
-
         User m_LoggedInUser;
 
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-
+            FacebookWrapper.FacebookService.s_CollectionLimit = 1000;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
@@ -48,6 +38,7 @@ namespace AppUI
 
         private void fetchEvents()
         {
+            listBoxEvents.HorizontalScrollbar = true;
             listBoxEvents.DisplayMember = "Name";
             foreach (Event fbEvent in m_LoggedInUser.Events)
             {
@@ -56,7 +47,7 @@ namespace AppUI
 
             if (m_LoggedInUser.Events.Count == 0)
             {
-                MessageBox.Show("No Events to retrieve :(");
+                MessageBox.Show("No Events yet");
             }
         }
 
@@ -67,7 +58,7 @@ namespace AppUI
 
         private void loginAndInit()
         {
-            LoginResult result = FacebookService.Login("904603836301816", "public_profile", "user_events", "user_about_me", "user_birthday", "user_hometown");
+            LoginResult result = FacebookService.Login("904603836301816", "user_friends", "email", "user_likes", "publish_actions", "user_posts", "public_profile", "user_events", "user_about_me", "user_birthday", "user_hometown");
 
             if (!string.IsNullOrEmpty(result.AccessToken))
             {
@@ -82,20 +73,102 @@ namespace AppUI
 
         private void fetchUserInfo()
         {
+            textBoxPost.Text = "What's on your mind...";
             fetchEvents();
             fetchUserData();
+            fetchPosts();
+            fetchPages();
+            fetchCheckIn();
+        }
+
+        private void fetchPages()
+        {
+            listBoxPages.HorizontalScrollbar = true;
+            listBoxPages.DisplayMember = "Name";
+            foreach (Page fbPage in m_LoggedInUser.LikedPages)
+            {
+                listBoxPages.Items.Add(fbPage);
+            }
+
+            if (m_LoggedInUser.LikedPages.Count == 0)
+            {
+                MessageBox.Show("You don't like any page");
+            }
+
+        }
+
+        private void fetchCheckIn()
+        {
+            listBoxCheckIn.HorizontalScrollbar = true;
+            listBoxCheckIn.DisplayMember = "Message";
+            foreach (Checkin fbCheckin in m_LoggedInUser.Checkins)
+            {
+                listBoxCheckIn.Items.Add(fbCheckin);
+            }
+
+            if (m_LoggedInUser.Checkins.Count == 0)
+            {
+                MessageBox.Show("You didn't do any check in");
+            }
+        }
+
+        private void fetchPosts()
+        {
+            listBoxFeed.HorizontalScrollbar = true;
+            foreach (Post post in m_LoggedInUser.Posts)
+            {
+                if (post.Message != null)
+                {
+                    listBoxFeed.Items.Add(post.UpdateTime + ": " + post.Message);
+                }
+                else if (post.Caption != null)
+                {
+                    listBoxFeed.Items.Add(post.UpdateTime + ": " + post.Caption);
+                }
+                else
+                {
+                    listBoxFeed.Items.Add(string.Format(post.UpdateTime + ": " + "[{0}]", post.Type));
+                }
+            }
+
+            if (m_LoggedInUser.Posts.Count == 0)
+            {
+                MessageBox.Show("No Posts to retrieve :(");
+            }
         }
 
         private void fetchUserData()
         {
-            pictureBox1.LoadAsync(m_LoggedInUser.PictureNormalURL);
-            textBoxPost.Text = "What's on your mind...";
+            listBoxProfie.HorizontalScrollbar = true;
+            pictureBoxProfile.LoadAsync(m_LoggedInUser.PictureNormalURL);
             listBoxProfie.Items.Add("Birthday: " + m_LoggedInUser.Birthday);
             listBoxProfie.Items.Add("Gender: " + m_LoggedInUser.Gender);
-            listBoxProfie.Items.Add("Hometown: " + m_LoggedInUser.Hometown);
+            listBoxProfie.Items.Add("Hometown: " + m_LoggedInUser.Hometown.Name);
+            listBoxProfie.Items.Add("Email: " + m_LoggedInUser.Email);
+            listBoxProfie.Items.Add("Languages: " + m_LoggedInUser.Languages);
         }
 
         private void listBoxProfie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPost_Click(object sender, EventArgs e)
+        {
+            Status postedStatus = m_LoggedInUser.PostStatus(textBoxPost.Text);
+            MessageBox.Show("Status: " + postedStatus.Message + " Posted");
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listBoxGroups_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
